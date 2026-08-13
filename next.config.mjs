4efad57d051907cc4e120a41d4a21d4eb42222fc
Ishaq Dasti <idasti039@gmail.com>
2026-08-13 17:18:37 -0400

    /youth redirect

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/youth",
+        destination:
+          "https://portal.icomd.org/nx/portal/neonevents/events?path=%2Fportal%2Fevents%2F54606",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
